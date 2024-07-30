912ff81abb69853d7b784e04d850d7b062abb1b7
Louis <22097871@student.westernsydney.edu.au>
2024-07-31 01:04:17 +1000

    commit

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,3 +1,4 @@
+
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
@@ -9,15 +10,43 @@ const openai = new OpenAI({
 
 export async function POST(req: Request) {
     // Extract the `messages` from the body of the request
-    const { messages } = await req.json();
+    const { messages, fileData } = await req.json();
+    const currentMessageContent = messages[messages.length-1].content;
 
+    let TEMPlATE;
+    
+
+    if (fileData) {
+        TEMPlATE = `
+        Using the context provided, answer the question.
+        Context: ${JSON.stringify(fileData)}
+        Question: ${JSON.stringify(currentMessageContent)}
+        `;
+    } else {
+        TEMPlATE = currentMessageContent;
+    }
+
+    messages[messages.length-1].content = TEMPlATE;
     // Request the OpenAI API for the response based on the prompt
     const response = await openai.chat.completions.create({
         model: 'gpt-4o',
+        temperature: 1.0,
         stream: true,
-        messages: messages,
-    });
+        messages:[
+            {
+                role: 'system', 
+                content: `You are a knowledgeable and professional high school teacher. You will provide detailed feedback and advice to Year 11 and Year 12 students on their draft essays or assignments.
+                        Start by acknowledging the effort put into the draft and highlight a positive aspect of the essay.
+                        List all the spelling and grammar errors found in the text.
+                        Check that referencing is provided in a consistent format and ensure that the referencing choices include in-text references.
+                        Provide feedback on the structure and flow of the essay.
+                        Offer any advice on how to improve the essay or assignment before final submission.
+                        No yapping, just get to the point.`, 
+            },
 
+        ...messages] ,
+    });
+    console.log(response);
     // Convert the response into a friendly text-stream
     const stream = OpenAIStream(response);
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 
 // import { currentUser } from "@clerk/nextjs/server";
 import { Chat } from "./components/chat";
+//import { UploadFile } from "./api/uploadFile";
 
 export const runtime = 'edge';
 
 export default function Page() {
   return <Chat />;
+  // <UploadFile/>;
 }
