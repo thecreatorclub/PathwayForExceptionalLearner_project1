276554762a4dbef6671b831761d26b2842bab33a
QuangNguyen <dongnguyen12122003@gmail.com>
2024-08-06 21:04:55 +1000

    change package

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 
-
 const isProtectedRoute = createRouteMatcher([
   "/words(.*)"
 ])
@@ -12,5 +11,9 @@ export default clerkMiddleware((auth,req) =>{
   }
 });
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [// Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
