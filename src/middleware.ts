906d78642137a5c48eb0a6f54be25b9d30bd57d0
QuangNguyen <dongnguyen12122003@gmail.com>
2024-08-06 23:11:54 +1000

    fixed the src middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,7 @@ const isProtectedRoute = createRouteMatcher([
   "/words(.*)"
 ])
 
-export default clerkMiddleware((auth,req) =>{
-  if (isProtectedRoute(req)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware();
 export const config = {
   matcher: [// Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
