3842fb3a773fa5bdca1c3d2e4c92c2f4a51dd2d0
Tomas Trescak <tomi.trescak@gmail.com>
2024-10-04 11:23:32 +1000

    Fixed build errors

--- src/app/api/upload/fileUtils.ts
+++ src/app/api/upload/fileUtils.ts
@@ -1,12 +1,12 @@
-import pdfToText from "react-pdftotext";
+// import pdfToText from "react-pdftotext";
 
-export async function readFileContent(file: File): Promise<string> {
-  const fileType = file.type;
-    try{
-        const text = await pdfToText(file);
-        return text;
-    } catch (error){
-        console.error(error);
-        return "";
-    }
-};
+// export async function readFileContent(file: File): Promise<string> {
+//   const fileType = file.type;
+//     try{
+//         const text = await pdfToText(file);
+//         return text;
+//     } catch (error){
+//         console.error(error);
+//         return "";
+//     }
+// };
--- src/app/signup/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <SignUp />;
-    </div>
-  )
-}
