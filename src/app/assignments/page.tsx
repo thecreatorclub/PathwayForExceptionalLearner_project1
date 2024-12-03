2b13b4ceca2d2c97d2492ab6ec1242c0999aa29b
Tomas Trescak <tomi.trescak@gmail.com>
2024-12-03 15:00:46 +1100

    Cleaned up student assignment list

--- src/app/assignments/page.tsx
+++ src/app/assignments/page.tsx
@@ -1,16 +1,77 @@
-import { ModeToggle } from "@/components/dark-mode-toggle";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Assignment } from "@prisma/client";
+import { CaretRightIcon } from "@radix-ui/react-icons";
+import {
+  Avatar,
+  Box,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  Text,
+} from "@radix-ui/themes";
 import Link from "next/link";
+
 import { getAssignments } from "../api/assignment/route";
 
-interface Assignment {
-  id: number;
-  title: string;
-  subject: string;
-  learningOutcomes: string;
-  markingCriteria: string;
-  createdAt: string;
-  updatedAt: string;
+function AssignmentCard({ assignment }: { assignment: Assignment }) {
+  return (
+    <Link
+      key={assignment.id}
+      className="assignments-link "
+      href={`/assignment/${assignment.id}`}
+    >
+      <Box width="100%">
+        <Card size="1" className="dark:hover:bg-slate-900">
+          <Flex gap="3" align="center">
+            <Avatar
+              size="3"
+              radius="full"
+              fallback={assignment.subject[0] || "S"}
+              color="indigo"
+            />
+            <Box className="flex-1">
+              <Text as="div" size="2" weight="bold">
+                {assignment.title}
+              </Text>
+              <Text as="div" size="2" color="gray">
+                {assignment.subject} &middot; updated{" "}
+                {assignment.updatedAt.toDateString()}
+              </Text>
+            </Box>
+            <CaretRightIcon height={30} width={30} />
+          </Flex>
+        </Card>
+      </Box>
+    </Link>
+  );
+}
+
+function AssignmentGroups({ assignments }: { assignments: Assignment[] }) {
+  const grouped = assignments.reduce((acc, assignment) => {
+    let group = acc.find((item) => item.key === assignment.subject);
+    if (!group) {
+      group = { key: assignment.subject, values: [] };
+      acc.push(group);
+    }
+    group.values.push(assignment);
+    return acc;
+  }, [] as Array<{ key: string; values: Assignment[] }>);
+
+  return (
+    <Flex gap="6" direction="column">
+      {grouped.map((group) => (
+        <Flex gap="3" direction="column" key={group.key}>
+          <Heading as="h3" className="text-xl">
+            {group.key}
+          </Heading>
+
+          {group.values.map((assignment) => (
+            <AssignmentCard key={assignment.id} assignment={assignment} />
+          ))}
+        </Flex>
+      ))}
+    </Flex>
+  );
 }
 
 export default async function AssignmentListPage() {
@@ -18,60 +79,11 @@ export default async function AssignmentListPage() {
   const assignments = await getAssignments();
 
   return (
-    <div className="assignment-page">
-      <div className="assignment-container">
-        <div className="assignment-list">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Link href="/">
-              <h1 className=" text-xl" style={{ cursor: "pointer" }}>
-                Home
-              </h1>
-            </Link>
-            <ThemeProvider>
-              <ModeToggle />
-            </ThemeProvider>
-          </div>
-          <h2>Assignment List</h2>
-          <ul>
-            {assignments.map((assignment) => (
-              <li key={assignment.id} className="assignment-item">
-                <p>
-                  <strong>Title:</strong> {assignment.title}
-                </p>
-                <p>
-                  <strong>Subject:</strong> {assignment.subject}
-                </p>
-                <p>
-                  <strong>Learning Outcomes:</strong>
-                </p>
-                <pre style={{ whiteSpace: "pre-wrap" }}>
-                  {assignment.learningOutcomes}
-                </pre>
-                <p>
-                  <strong>Marking Criteria:</strong>
-                </p>
-                <pre style={{ whiteSpace: "pre-wrap" }}>
-                  {assignment.markingCriteria}
-                </pre>
-                <div>
-                  <Link
-                    className="assignments-link"
-                    href={`/assignment/${assignment.id}`}
-                  >
-                    View Details
-                  </Link>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Container className="p-8">
+      <Heading as="h1" className="mb-4">
+        Assignment List
+      </Heading>
+      <AssignmentGroups assignments={assignments} />
+    </Container>
   );
 }
